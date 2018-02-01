91dec68429e542b1388482e62834e60940d3039e
yangchao <yangchao01@doctorwork.com>
2018-02-01 17:56:58 +0800

    update testcase

--- src/test/java/com/imadcn/system/test/idworker/ManualCloseTest.java
+++ src/test/java/com/imadcn/system/test/idworker/ManualCloseTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import com.imadcn.framework.idworker.generator.SnowflakeGenerator;
 import com.imadcn.system.test.spring.AbstractZookeeperJUnit4SpringContextTests;
 
-@ContextConfiguration(locations = "classpath:META-INF/idworker-ctx.xml")
+@ContextConfiguration(locations = "classpath:META-INF/idworker-ctx-manual.xml")
 public final class ManualCloseTest extends AbstractZookeeperJUnit4SpringContextTests {
 
 	@Autowired
