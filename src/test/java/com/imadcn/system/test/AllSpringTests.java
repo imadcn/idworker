cd06d2748f8840b4a37a53622bc2b552715f4738
yangchao <yangchao01@doctorwork.com>
2018-02-01 17:45:59 +0800

    update testcase

--- src/test/java/com/imadcn/system/test/AllSpringTests.java
+++ src/test/java/com/imadcn/system/test/AllSpringTests.java
@@ -4,11 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.imadcn.system.test.idworker.ManualCloseTest;
 import com.imadcn.system.test.idworker.SpringIdWorkerTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ SpringIdWorkerTest.class, ManualCloseTest.class })
+@SuiteClasses({ SpringIdWorkerTest.class})
 public final class AllSpringTests {
 
 }
