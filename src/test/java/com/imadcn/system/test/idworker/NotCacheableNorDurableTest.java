1af46a81482a2c6eca2ac7e72bbeb794b52a3eed
imadcn <nevase@foxmail.com>
2022-07-11 14:10:09 +0800

    Squashed commit of the following:
    commit b845a866c4fd432f6e5fff2cb85c4facfdfdece4
    Author: imadcn <nevase@foxmail.com>
    Date:   Mon Jul 11 14:07:44 2022 +0800
    
        Squashed commit of the following:
    
        commit 04110b9c6d1f66579943c6550dfcac4c0a3c8256
        Merge: c882e04 ffaaf3c
        Author: imadcn <nevase@foxmail.com>
        Date:   Mon Jul 11 14:07:28 2022 +0800
    
            Merge branch 'master' of https://github.com/imadcn/idworker
    
        commit ffaaf3c2a97257b5aa4a73cad07939665247a9a5
        Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
        Date:   Mon Jul 11 14:07:11 2022 +0800
    
            Bump jackson-databind from 2.13.0 to 2.13.2.1 (#17)
    
            Bumps [jackson-databind](https://github.com/FasterXML/jackson) from 2.13.0 to 2.13.2.1.
            - [Release notes](https://github.com/FasterXML/jackson/releases)
            - [Commits](https://github.com/FasterXML/jackson/commits)
    
            ---
            updated-dependencies:
            - dependency-name: com.fasterxml.jackson.core:jackson-databind
              dependency-type: direct:production
            ...
    
            Signed-off-by: dependabot[bot] <support@github.com>
    
            Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
        commit 32e4c7b4867d1b4493530823cbb461fefe8ea47d
        Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
        Date:   Fri Jun 17 15:54:54 2022 +0800
    
            Bump fastjson from 1.2.78 to 1.2.83 (#16)
    
            Bumps [fastjson](https://github.com/alibaba/fastjson) from 1.2.78 to 1.2.83.
            - [Release notes](https://github.com/alibaba/fastjson/releases)
            - [Commits](https://github.com/alibaba/fastjson/compare/1.2.78...1.2.83)
    
            ---
            updated-dependencies:
            - dependency-name: com.alibaba:fastjson
              dependency-type: direct:production
            ...
    
            Signed-off-by: dependabot[bot] <support@github.com>
    
            Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    commit 52f2eeb4a2fff50dcd38b01d44a232ee13a92c09
    Author: imadcn <nevase@foxmail.com>
    Date:   Tue May 31 15:23:35 2022 +0800
    
        fix err
    
    commit 2abcd79312cb3761335ede70beb13984b62b0374
    Author: imadcn <nevase@foxmail.com>
    Date:   Tue May 31 15:17:11 2022 +0800
    
        fix err

--- src/test/java/com/imadcn/system/test/idworker/NotCacheableNorDurableTest.java
+++ src/test/java/com/imadcn/system/test/idworker/NotCacheableNorDurableTest.java
@@ -19,8 +19,8 @@ import org.springframework.test.context.ContextConfiguration;
 import com.imadcn.framework.idworker.generator.SnowflakeGenerator;
 import com.imadcn.system.test.spring.AbstractZookeeperJUnit4SpringContextTests;
 
-@ContextConfiguration(locations = "classpath:META-INF/idworker-ctx-cachable-and-durable.xml")
-public final class CachableAndtDurableTest extends AbstractZookeeperJUnit4SpringContextTests {
+@ContextConfiguration(locations = "classpath:META-INF/idworker-ctx-not-cacheable-nor-durable.xml")
+public final class NotCacheableNorDurableTest extends AbstractZookeeperJUnit4SpringContextTests {
 
     @Autowired
     private SnowflakeGenerator snowflakeGenerator;
